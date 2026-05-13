7fb9cf30af14c0bca0cd10a43372959cf8468af3
vn-24k <manuellesilva0305@gmail.com>
2026-05-13 17:35:02 -0300

    🚨 DEPLOY FINAL: Forçando pasta actions

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,6 +1,5 @@
 "use server";
 import { auth, currentUser } from "@clerk/nextjs/server";
-
 export async function syncUser() {
   const { userId } = await auth();
   const user = await currentUser();
